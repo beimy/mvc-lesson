f04abe964abec264ece453d17570eaeda5c19706
Beimy <arbm159357@gmail.com>
2022-05-28 15:25:58 -0400

    added login/ logout feature

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 const hbs = exphbs.create({});
  
 app.engine('handlebars', hbs.engine);
@@ -15,6 +29,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 app.use(require('./controllers/'));
 
